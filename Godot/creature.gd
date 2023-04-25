829feca862e30615524a50a0fbaec3530ec512d7
Parsley3000 <peterkeane2@gmail.com>
2023-04-25 10:38:02 +0100

    commit 2

--- Godot/creature.gd
+++ Godot/creature.gd
@@ -1,13 +1,20 @@
 extends Spatial
 
 
-func _ready():
-	pass # Replace with function body.
+onready var player = $"../Player"
+onready var pod = $Pod
 
-func _physics_process(delta):
-	pass
-	
-
+var player_in_pod = false
 
 func _on_Area_body_entered(body):
-	print("hello")
+	if body == player:
+		player.global_transform.origin = pod.global_transform.origin + Vector3(0, 1, 0)
+		
+		player_in_pod = true
+
+func _process(delta):
+	if player_in_pod:
+		var target_transform = player.global_transform
+		global_transform = target_transform
+
+		rotation = player.rotation
